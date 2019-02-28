d9cdffb870193fde230f80685b5662858ea9fafa
Chandler Childs <rubberband75@gmail.com>
2019-02-27 19:10:32 -0700

    add email test

---
+++ app/src/test/java/com/rubberband75/recivoir/EmailValidatorTest.java
@@ -0,0 +1,11 @@
+package com.rubberband75.recivoir;
+
+//import com.google.common.truth.Truth.assertThat;
+import org.junit.Test;
+
+public class EmailValidatorTest {
+//    @Test
+//    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+//        assertThat(EmailValidator.isValidEmail("name@email.com")).isTrue();
+//    }
+}

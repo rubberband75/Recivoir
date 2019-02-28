8c5243710a3756a16f2d6bb3e9cfc12e88a535f0
Chandler Childs <rubberband75@gmail.com>
2019-02-27 20:32:30 -0700

    add test stubs

--- app/src/test/java/com/rubberband75/recivoir/EmailValidatorTest.java
+++ app/src/test/java/com/rubberband75/recivoir/EmailValidatorTest.java
@@ -1,11 +1,68 @@
 package com.rubberband75.recivoir;
 
-//import com.google.common.truth.Truth.assertThat;
 import org.junit.Test;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+import android.content.Context;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class EmailValidatorTest {
-//    @Test
-//    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
-//        assertThat(EmailValidator.isValidEmail("name@email.com")).isTrue();
-//    }
+
+    private static final String FAKE_STRING = "HELLO WORLD";
+
+    @Mock
+    Context mockContext;
+
+    @Test
+    public void readStringFromContext_LocalizedString() {
+//        // Given a mocked Context injected into the object under test...
+//        when(mockContext.getString(R.string.hello_world))
+//                .thenReturn(FAKE_STRING);
+//        ClassUnderTest myObjectUnderTest = new ClassUnderTest(mockContext);
+//
+//        // ...when the string is returned from the object under test...
+//        String result = myObjectUnderTest.getHelloWorldString();
+//
+//        // ...then the result should be the expected one.
+//        assertThat(result, is(FAKE_STRING));
+    }
+
+
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+
+        assertThat("abc").isEqualTo(123);
+    }
+
+    @Test
+    public void verifyRecipeLoader() {
+//        int recipeID = 123;
+
+//        Recipe r = Model.getRecipeFromDB(recipeID);
+
+//        assertThat(r.title = "Test Recipe");
+
+        assertThat("recipe").isEqualTo("recipeFromDB");
+    }
+
+
+    @Test
+    public void verifyLogin() {
+        String username = "rubberband75";
+        String password = "baconPancakes";
+
+//        User u = login(username, password);
+
+//        assert
+
+        }
+
+    }
 }

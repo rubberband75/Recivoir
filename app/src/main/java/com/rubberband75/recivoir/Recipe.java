d8ebe9a6f3a908a89d1f328e2c8a898b0898fa3c
Chandler Childs <rubberband75@gmail.com>
2019-03-20 18:45:43 -0600

    add recipe class

---
+++ app/src/main/java/com/rubberband75/recivoir/Recipe.java
@@ -0,0 +1,34 @@
+package com.rubberband75.recivoir;
+
+/**
+ * @author Chandler, Michael, Sam
+ * @version 1.0
+ *
+ * Recipe Class
+ */
+public class Recipe {
+    private String title;
+
+    /**
+     * Recipe Constructor
+     */
+    public Recipe() {
+
+    }
+
+    /**
+     * Gets title
+     * @return title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the title of recipe
+     * @param title Title of recipe
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

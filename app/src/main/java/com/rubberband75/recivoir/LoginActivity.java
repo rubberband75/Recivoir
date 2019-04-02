91790142081fc2336279d8d28cbc25e7ecfab395
saerca <cas16036@byui.edu>
2019-04-01 23:23:29 -0400

    Completed Login Activity

--- app/src/main/java/com/rubberband75/recivoir/LoginActivity.java
+++ app/src/main/java/com/rubberband75/recivoir/LoginActivity.java
@@ -33,6 +33,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -43,6 +44,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,9 +53,10 @@ import static android.Manifest.permission.READ_CONTACTS;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>, View.OnClickListener {
     private static String TAG = "Recivoir - LoginActivity";
     private FirebaseAuth mAuth;
+    private static final int RC_SIGN_IN = 1001;
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -77,6 +80,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private GoogleSignInClient mGoogleSignInClient;
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -144,17 +150,19 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        FirebaseUser user;
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
+                            user = mAuth.getCurrentUser();
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
-                            updateUI(null);
+                            Snackbar.make(findViewById(R.id.sign_in_button), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            user = null;
                         }
+                        updateUI(user);
 
                         // ...
                     }
@@ -167,6 +175,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             case R.id.sign_in_button:
                 signIn();
                 break;
+            case R.id.sign_out_and_disconnect:
+                signOut();
+                break;
             // ...
         }
     }
@@ -174,6 +185,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
     }
 
     @Override
@@ -207,18 +223,28 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
+            updateUI((GoogleSignInAccount) null);
         }
     }
 
     private void updateUI(FirebaseUser currentUser) {
-        if (currentUser != null)
-            ;
+        if (currentUser != null) {
+            mStatusTextView.setText(getString(R.string.google_status_fmt, currentUser.getEmail()));
+            mDetailTextView.setText(getString(R.string.firebase_status_fmt, currentUser.getUid()));
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
     }
 
     private void updateUI(GoogleSignInAccount account) {
-        if (account != null)
-            ;
+
     }
 
 

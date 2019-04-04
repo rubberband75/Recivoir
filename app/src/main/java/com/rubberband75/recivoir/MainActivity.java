5b39dbf2ae34769fbef4915adea4cf93e918f99b
Chandler Childs <rubberband75@gmail.com>
2019-04-04 15:45:45 -0600

    created new main activity

--- app/src/main/java/com/rubberband75/recivoir/MainActivity.java
+++ app/src/main/java/com/rubberband75/recivoir/MainActivity.java
@@ -1,72 +1,185 @@
 package com.rubberband75.recivoir;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static String TAG = "Recivoir - MainActivity";
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+
+    private static String TAG = "[Recivoir]MainActivity:";
+
+    GoogleApiClient mGoogleApiClient;
+
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d(TAG, "The creation has occured");
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        findViewById(R.id.login_button).setOnClickListener(this);
-    }
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
-    @Override
-    protected void onStop() {
-        // call the superclass method first
-        super.onStop();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        EditText nameInput = findViewById(R.id.name_input);
-        String name = nameInput.getText().toString();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        SharedPreferences userDetails = this.getSharedPreferences("userdetails", MODE_PRIVATE);
-        SharedPreferences.Editor edit = userDetails.edit();
-        edit.putString("username", name);
-        edit.apply();
-        Log.d(TAG, "Saving name to shared preferences");
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-    }
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-        SharedPreferences userDetails = this.getSharedPreferences("userdetails", MODE_PRIVATE);
-
-        if(userDetails.contains("username")) {
-            String name = userDetails.getString("username", null);
-            EditText nameInput = (EditText) findViewById(R.id.name_input);
-            nameInput.setText(name);
-            Log.d(TAG, "Loading name from shared preferences");
+        if(account != null) {
+            Log.d(TAG, "Signed In");
+            updateUI(account);
         } else {
-            Log.d(TAG, "No name saved in shared preferences");
+            Log.d(TAG, "Forcing Login");
+            forceSignIn();
+        }
+
+//        FirebaseApp.initializeApp(this);
+    }
+
+
+
+    public void forceSignIn(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void signOut(){
+        Log.d(TAG, "Signing Out");
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                Log.d(TAG, "Signed Out");
+                forceSignIn();
+            }
+        });
+    }
+
+
+
+    private void updateUI(GoogleSignInAccount account) {
+        Log.d(TAG, "Update UI for account: " + account.getEmail());
+
+        ((TextView) findViewById(R.id.greeting_text)).setText("Welcome " + account.getGivenName());
+
+//        View headerView = navigationView.getHeaderView(0);
+//        ((TextView) headerView.findViewById(R.id.drawer_header_name)).setText(account.getDisplayName());
+//        ((TextView) headerView.findViewById(R.id.drawer_header_email)).setText(account.getEmail());
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
 
-    public void startLoginActivity() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        Log.d(TAG, "Starting Login Activity");
-        startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_new_recipes) {
+            // Handle the camera action
+        } else if (id == R.id.nav_my_recipes) {
+
+        } else if (id == R.id.nav_my_friends) {
+
+        } else if (id == R.id.nav_find_recipes) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_sign_out) {
+            signOut();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.login_button:
-                startLoginActivity();
-                break;
-            // ...
-        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
     @Override
---
+++ app/src/main/java/com/rubberband75/recivoir/MainActivityOld.java
@@ -0,0 +1,76 @@
+package com.rubberband75.recivoir;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivityOld extends AppCompatActivity implements View.OnClickListener {
+    private static String TAG = "Recivoir - MainActivityOld";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_old);
+        Log.d(TAG, "The creation has occured");
+
+        findViewById(R.id.login_button).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        // call the superclass method first
+        super.onStop();
+
+        EditText nameInput = findViewById(R.id.name_input);
+        String name = nameInput.getText().toString();
+
+        SharedPreferences userDetails = this.getSharedPreferences("userdetails", MODE_PRIVATE);
+        SharedPreferences.Editor edit = userDetails.edit();
+        edit.putString("username", name);
+        edit.apply();
+        Log.d(TAG, "Saving name to shared preferences");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences userDetails = this.getSharedPreferences("userdetails", MODE_PRIVATE);
+
+        if(userDetails.contains("username")) {
+            String name = userDetails.getString("username", null);
+            EditText nameInput = (EditText) findViewById(R.id.name_input);
+            nameInput.setText(name);
+            Log.d(TAG, "Loading name from shared preferences");
+        } else {
+            Log.d(TAG, "No name saved in shared preferences");
+        }
+    }
+
+
+    public void startLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        Log.d(TAG, "Starting Login Activity");
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                startLoginActivity();
+                break;
+            // ...
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}

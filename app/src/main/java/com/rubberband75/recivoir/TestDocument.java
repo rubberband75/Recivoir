23bfe3fcac6ff8962a82c7830630ef195e52a413
Chandler Childs <rubberband75@gmail.com>
2019-04-05 15:01:10 -0600

    update testDoc

--- app/src/main/java/com/rubberband75/recivoir/TestDocument.java
+++ app/src/main/java/com/rubberband75/recivoir/TestDocument.java
@@ -1,6 +1,8 @@
 package com.rubberband75.recivoir;
 
 public class TestDocument {
-    public String title = "";
-    public String ingredients = "";
+    public String title = "Cool Recipe Title";
+    public String ingredients = "chips\ncorn\ncheese\nbeans\ntortilla";
+    public String steps = "make the food";
+    public String notes = "you don't need any notes";
 }

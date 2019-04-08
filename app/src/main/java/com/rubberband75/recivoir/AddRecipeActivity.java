1d525f7c8c90be030f01c4c6b517ac7053b024e9
Unknownsage13 <Mit14001@byui.edu>
2019-04-08 19:53:35 -0400

    Added the activities

--- app/src/main/java/com/rubberband75/recivoir/AddRecipeActivity.java
+++ app/src/main/java/com/rubberband75/recivoir/AddRecipeActivity.java
@@ -1,11 +1,11 @@
 package com.rubberband75.recivoir;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -22,6 +22,13 @@ public class AddRecipeActivity extends AppCompatActivity {
 
     private static final String TAG = "[Recivoir]AddRecipe:";
 
+    private static final String FIRESTORE_COLLECTION = "public";
+    private static final String TITLE_KEY = "title";
+    private static final String INGREDIENTS_KEY = "ingredients";
+    private static final String STEPS_KEY = "steps";
+    private static final String NOTES_KEY = "notes";
+
+
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
@@ -33,40 +40,54 @@ public class AddRecipeActivity extends AppCompatActivity {
     }
 
     public void saveRecipe(View view) {
+        Log.d(TAG, "saveRecipe: SAVE Button Press");
+        Toast.makeText(this, "SAVE Button Press", Toast.LENGTH_SHORT).show();
+
+
+          /* This works, it just needs to be adapted to the right inputs */
+
         EditText titleInput = (EditText) findViewById(R.id.new_title_input);
         EditText ingredientsInput = (EditText) findViewById(R.id.new_ingredients_input);
         EditText stepsInput = (EditText) findViewById(R.id.new_steps_input);
         EditText notesInput = (EditText) findViewById(R.id.new_notes_input);
+        CheckBox publicInput = (CheckBox) findViewById(R.id.publicBox);
 
         String titleText = titleInput.getText().toString();
         String ingredientsText = ingredientsInput.getText().toString();
         String stepsText = stepsInput.getText().toString();
         String notesText = notesInput.getText().toString();
+        Boolean publicStatus = publicInput.isChecked();
 
         if(titleText.isEmpty() || ingredientsText.isEmpty() || stepsText.isEmpty()) {
             Toast.makeText(this, "Missing Info", Toast.LENGTH_SHORT).show();
             return;
         }
 
+        Map<String, Object> recipe = new HashMap<>();
+        recipe.put(TITLE_KEY, titleText);
+        recipe.put(INGREDIENTS_KEY, ingredientsText);
+        recipe.put(STEPS_KEY, stepsText);
+        recipe.put(NOTES_KEY, notesText);
+        recipe.put(FIRESTORE_COLLECTION, publicStatus);
+
+
         Toast.makeText(this, "Saving Recipe", Toast.LENGTH_SHORT).show();
 
-        final Context context = this;
-        Database.saveRecipe(titleText, ingredientsText, stepsText, notesText, true)
-            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                @Override
-                public void onSuccess(DocumentReference documentReference) {
-                    Toast.makeText(context, "Recipe Saved", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    finish();
-                }
-            })
-            .addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(context, "An Error Occured", Toast.LENGTH_SHORT).show();
-                    Log.w(TAG, "Error adding document", e);
-                }
-            });
+        db.collection(FIRESTORE_COLLECTION)
+                .add(recipe)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 
     public void discardRecipe(View view) {

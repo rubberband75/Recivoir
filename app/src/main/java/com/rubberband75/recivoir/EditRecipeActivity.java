eb69e061f495bcfd9abf7979b8b24d09b9864460
Unknownsage13 <Mit14001@byui.edu>
2019-04-09 13:54:15 -0400

    Added the activities

--- app/src/main/java/com/rubberband75/recivoir/EditRecipeActivity.java
+++ app/src/main/java/com/rubberband75/recivoir/EditRecipeActivity.java
@@ -2,12 +2,35 @@ package com.rubberband75.recivoir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditRecipeActivity extends AppCompatActivity {
 
+    String editTitle;
+    String editIngredients;
+    String editSteps;
+    String editNotes;
+
+    public EditRecipeActivity(Recipe input){
+        editTitle = input.getTitle();
+        editIngredients = input.getIngredients();
+        editSteps = input.getSteps();
+        editNotes = input.getNotes();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_recipe);
+
+        EditText editTitleText = (EditText) findViewById(R.id.editRecipeTitle);
+        EditText editIngredientsText = (EditText) findViewById(R.id.editRecipeIngredients);
+        EditText editStepsText = (EditText) findViewById(R.id.editRecipeSteps);
+        EditText editNotesText = (EditText) findViewById(R.id.editRecipeNotes);
+
+        editTitleText.setText(editTitle);
+        editIngredientsText.setText(editIngredients);
+        editStepsText.setText(editSteps);
+        editNotesText.setText(editNotes);
     }
+
 }

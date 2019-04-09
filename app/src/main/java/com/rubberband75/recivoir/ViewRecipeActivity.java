18c4a8c09911a4a0bae2242517149f0785792751
Chandler Childs <rubberband75@gmail.com>
2019-04-09 01:46:28 -0600

    ViewRecipeActivity now pulls data from recipeID

--- app/src/main/java/com/rubberband75/recivoir/ViewRecipeActivity.java
+++ app/src/main/java/com/rubberband75/recivoir/ViewRecipeActivity.java
@@ -6,10 +6,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+
 public class ViewRecipeActivity extends AppCompatActivity {
+    private static final String TAG = "[Recivoir]ViewRecipes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +36,31 @@ public class ViewRecipeActivity extends AppCompatActivity {
         Intent incomingIntent = getIntent();
         String recipeID = incomingIntent.getStringExtra("recipeID");
 
-        Recipe recipe = Database.getRecipe(recipeID);
+        Database.getRecipe(recipeID).addOnSuccessListener(new  OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
 
-        String titleText = recipe.getTitle();
-        TextView titleView = findViewById(R.id.recipeTitleView);
-        titleView.setText(titleText);
+                Recipe recipe = new Recipe(documentSnapshot);
+                Log.d(TAG, "onSuccess: " + recipe.getRecipeID() + " | " + recipe.getTitle());
 
-        String ingredientsText = recipe.getIngredients();
-        TextView ingredientsView = findViewById(R.id.recipeIngredientsView);
-        ingredientsView.setText(ingredientsText);
+                String titleText = recipe.getTitle();
+                TextView titleView = findViewById(R.id.recipeTitleView);
+                titleView.setText(titleText);
 
-        String instructionsText = recipe.getSteps();
-        TextView instructionsView = findViewById(R.id.recipeInstructionsView);
-        instructionsView.setText(instructionsText);
+                String ingredientsText = recipe.getIngredients();
+                TextView ingredientsView = findViewById(R.id.recipeIngredientsView);
+                ingredientsView.setText(ingredientsText);
 
-        String notesText = recipe.getNotes();
-        TextView notesView = findViewById(R.id.recipeNotesView);
-        notesView.setText(notesText);
+                String instructionsText = recipe.getSteps();
+                TextView instructionsView = findViewById(R.id.recipeInstructionsView);
+                instructionsView.setText(instructionsText);
 
+                String notesText = recipe.getNotes();
+                TextView notesView = findViewById(R.id.recipeNotesView);
+                notesView.setText(notesText);
+
+            }
+        });
 
     }
 

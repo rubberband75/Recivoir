d292f09bb286ae3347d2f4810437bcd176737b2b
saerca <cas16036@byui.edu>
2019-04-08 22:39:52 -0400

    Started update for MyFriendsActivity

--- app/src/main/java/com/rubberband75/recivoir/MyFriendsActivity.java
+++ app/src/main/java/com/rubberband75/recivoir/MyFriendsActivity.java
@@ -1,11 +1,19 @@
 package com.rubberband75.recivoir;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class MyFriendsActivity extends AppCompatActivity {
     private static final String TAG = "[Recivoir]MyFriends";
@@ -15,7 +23,21 @@ public class MyFriendsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_friends);
 
+        Database.getUsers().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                Log.d(TAG, "onComplete: getMyRecipes()");
+                //Then within this, call the appropriate get____fromTask(task) function
+                //If it's a list of recipes, it's this
+                //But there will also be one for Users
+                ArrayList<Recipe> recipes = Database.getRecipesFromTask(task);
 
+                for(Recipe r : recipes) {
+                    Log.d(TAG, "onComplete: " + r.getTitle());
+                }
+
+            }
+        });
     }
 
 }

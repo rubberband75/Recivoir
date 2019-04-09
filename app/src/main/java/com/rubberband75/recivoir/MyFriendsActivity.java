07fea3928dc794220e01184c8d5b9eaff5fd3839
saerca <cas16036@byui.edu>
2019-04-09 14:29:26 -0400

    Removed unnecessary imports from MyFriendsActivity

--- app/src/main/java/com/rubberband75/recivoir/MyFriendsActivity.java
+++ app/src/main/java/com/rubberband75/recivoir/MyFriendsActivity.java
@@ -2,12 +2,8 @@ package com.rubberband75.recivoir;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;

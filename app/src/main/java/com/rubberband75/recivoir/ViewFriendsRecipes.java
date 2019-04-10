3f09503286a072c5d81022096b8ef7fb1ecfbf10
saerca <cas16036@byui.edu>
2019-04-10 01:02:17 -0400

    Created Activities to view and add Friends

---
+++ app/src/main/java/com/rubberband75/recivoir/ViewFriendsRecipes.java
@@ -0,0 +1,13 @@
+package com.rubberband75.recivoir;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewFriendsRecipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_friends_recipes);
+    }
+}
